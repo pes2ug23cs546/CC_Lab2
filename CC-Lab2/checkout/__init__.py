989fc94bd09386144956db3aab42f81ac16fda34
PES2UG23CS546 <pes2ug23cs546@pesu.pes.edu>
2026-01-29 15:55:19 +0530

    PES2UG23CS546

---
+++ CC-Lab2/checkout/__init__.py
@@ -0,0 +1,14 @@
+from database import get_db
+
+def checkout_logic():
+    db = get_db()
+    db.row_factory = None  
+
+    events = db.execute("SELECT fee FROM events").fetchall()
+
+    total = 0
+    for e in events:
+        total += e[0]
+
+    return total
+
---
+++ CC-Lab2/locust/events_locustfile.py
@@ -0,0 +1,13 @@
+from locust import HttpUser, task, between
+
+class EventsUser(HttpUser):
+    wait_time = between(1, 2)
+
+    @task
+    def view_events(self):
+        self.client.get(
+            
+            "/events",
+            params={"user": "locust_user"},
+            name="/events"
+        )
